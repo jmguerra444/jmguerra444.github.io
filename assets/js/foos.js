c2141684d9db3d993f1bfa7d96f31099c6b711a1
jmguerra444 <guerrajorge93@gmail.com>
2019-08-03 20:13:21 +0200

    Fix side by side widget

--- assets/js/foos.js
+++ assets/js/foos.js
@@ -1,11 +1,10 @@
-
-<script>
-    function hide(elem_Id) {
+function hide(elem_Id) 
+{
     var x = document.getElementById(elem_Id);
+
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
-    }
-</script>
+}
