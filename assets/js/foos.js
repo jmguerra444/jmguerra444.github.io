a72f266bb5b4c59ac5cd28b7b074e11736066409
jmguerra444 <guerrajorge93@gmail.com>
2019-08-03 20:26:30 +0200

    Fix toggle widgets button

--- assets/js/foos.js
+++ assets/js/foos.js
@@ -1,7 +1,7 @@
 function hide(elem_Id) 
 {
-    var x = document.getElementById(elem_Id);
-
+    
+    var x = document.getElementById(elem_Id);   
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
