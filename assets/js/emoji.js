fb547b3879a8b6a35010f33a8069afde6a9e062e
Jorge Mario Guerra <guerrajorge93@gmail.com>
2020-04-28 22:37:43 -0500

    Reduce general font-size

--- assets/js/emoji.js
+++ assets/js/emoji.js
@@ -1,12 +1,22 @@
+
+var results = "";
+var counter = 0; // Controls the number of elements in the story
+
+var language = "en"
+var translations = "" //Results but translated
+
 var emojiList = $.getJSON( "assets/json/emoji.json", function( json )
 {
     emojiList = json;
 });
 
-var results = "";
-var counter = 0; // Controls the number of elements in the story
+var translationList = $.getJSON( "assets/json/translations.json", function( json )
+{
+    translationList = json;
+});
 
-function get_x(n) {
+function run() 
+{
     
     if (counter == 5) 
     {
@@ -14,46 +24,51 @@ function get_x(n) {
         counter = 0;
     }
     var pos = 0;
-    var myVar = setInterval(function() 
+    var interval = setInterval(function() 
     {
-        get_1(n);
+        get_1(2, pos, interval);
     }, 80);
+}
 
-    function get_1(n) 
+function get_translation(sel, item)
+{
+
+}
+
+function get_1(n, pos, interval) 
+{
+    var i;
+    var output = "";
+    
+    for (i = 0; i < n; i++) 
     {
-        var i;
-        var output = "";
-        
-        for (i = 0; i < n; i++) 
-        {
-            var item = emojiList[Math.floor(Math.random() * emojiList.length)];
-            output += item.emoji;
-        }
-        document.getElementById("demo").innerHTML = output;
-        
-        var lottery = false
-        if (item.category == "People & Body")
-        {
-            lottery = Math.random() < 0.2;
-        }
+        var item = emojiList[Math.floor(Math.random() * emojiList.length)];
+        output += item.emoji;
+    }
+    document.getElementById("demo").innerHTML = output;
+    
+    var lottery = false
+    if (item.category == "People & Body")
+    {
+        lottery = Math.random() < 0.2;
+    }
 
-        pos++;
-        if (pos == 40 || lottery)
+    pos++;
+    if (pos == 40 || lottery)
+    {
+        clearInterval(interval)
+        results += output
+        document.getElementById("story").innerHTML = results;
+        counter++;
+        
+        console.log(pos)
+        if (counter == 4)
         {
-            clearInterval(myVar)
-            results += output
-            document.getElementById("story").innerHTML = results;
-            counter++;
-            
-            console.log(pos)
-            if (counter == 4)
-            {
-                document.getElementById("alert").innerHTML = "Last!!";
-            }
-            if (counter == 5)
-            {
-                document.getElementById("alert").innerHTML = "";
-            }
+            document.getElementById("alert").innerHTML = "Last!!";
+        }
+        if (counter == 5)
+        {
+            document.getElementById("alert").innerHTML = "";
         }
     }
 }
