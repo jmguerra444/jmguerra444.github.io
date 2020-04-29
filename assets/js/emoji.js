79eb939d06083216c97ae4d910e54ac215119879
Jorge Mario Guerra <guerrajorge93@gmail.com>
2020-04-29 16:41:19 -0500

    Add tag translations

--- assets/js/emoji.js
+++ assets/js/emoji.js
@@ -1,8 +1,9 @@
 var pos = 0;
-var numStories = 5;
+var numStories = 7;
 var language = "en"
 var resultsList = [] // Contains the list of element results 
                      //TODO: refactor repeated variable
+var isRunning = false
 
 var emojiList = $.getJSON( "assets/json/emoji.json", function( json )
 {
@@ -14,8 +15,15 @@ var translationList = $.getJSON( "assets/json/translations.json", function( json
     translationList = json;
 });
 
+var tagList = $.getJSON( "assets/json/tag_translations.json", function( json )
+{
+    tagList = json;
+});
+
 function run() 
 {
+    if (isRunning) { return; }
+
     pos = 0;
     if (resultsList.length == numStories * 2)
     {
@@ -23,26 +31,36 @@ function run()
     }
     var interval = setInterval(function() 
     {
+        isRunning = true
         get(2, interval);
     }, 80);
+    console.log(interval);
 }
 
 function handleTranslation()
 {
     var translationString = ""
+    
     var e = document.getElementById("lang")
     language = e.options[e.selectedIndex].value;
     for (var i = 0; i < resultsList.length; i++)
     {
+        var tagString = ""
         var q = resultsList[i]["description"];
         var thisTranslation = translationList[q][language];
-        translationString += resultsList[i]["emoji"] + thisTranslation + "      ";
+        
         if (i == 0 || i == 1)
         {
-            translationString = "<strong>" + translationString + "</strong><br>";
+            tags = tagList[q][language];
+            tagString = tags.join(', ');
+            translationString += "<strong style='font-size: 130%'>" + resultsList[i]["emoji"] + thisTranslation + "&nbsp</strong><span>" + tagString + "</span><br>";
+        }
+        else
+        {
+            translationString += resultsList[i]["emoji"] + thisTranslation + "&nbsp";
         }
 
-        if (i >= 16) { break; }
+        if (i >= 20) { break; }
     }
     
     document.getElementById("translation-result").innerHTML = translationString;
@@ -101,7 +119,7 @@ function get(n, interval)
     
     document.getElementById("spin").innerHTML = item_1.emoji + item_2.emoji;
     
-    var lottery = false
+    var lottery = false;
     if (item_1.category == "People & Body" || item_2.category == "People & Body")
     {
         lottery = Math.random() < 0.2;
@@ -111,6 +129,8 @@ function get(n, interval)
     if (pos == 20)
     {
         clearInterval(interval)
+        isRunning = false;
+
         resultsList.unshift(item_1);
         resultsList.unshift(item_2);
         handleStoryline();
